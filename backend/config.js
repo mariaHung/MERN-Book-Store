01117477347a6b54283e83ae62902fc0cccbcd17
Maria Hung <mariarebecahung@gmail.com>
2024-05-29 20:51:36 +0200

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://rebeca:tK0vXqPpSe33wxMr@book-store-mern.mzmmkaa.mongodb.net/books-collections?retryWrites=true&w=majority&appName=Book-Store-Mern'
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import booksRoute from "./routes/booksRoutes.js"
+import cors from "cors";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("Welcome to MERN Stack")
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App conected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to pot ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
