4720e670e89c943d744e02edb1e909a400cafbc0
Maria Hung <mariarebecahung@gmail.com>
2024-05-29 21:05:28 +0200

    change to config

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 
-    'mongodb+srv://rebeca:tK0vXqPpSe33wxMr@book-store-mern.mzmmkaa.mongodb.net/books-collections?retryWrites=true&w=majority&appName=Book-Store-Mern'
+export const mongoDBURL = ""
